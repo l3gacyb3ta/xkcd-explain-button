5b46f7160bf86f967d0cd0d33579b21f069c8c34
L3gacy B3ta <l3gacy.b3ta@disroot.org>
2021-04-23 17:32:09 -0400

    Added some better comments.

--- xkcd.js
+++ xkcd.js
@@ -1,4 +1,11 @@
-var element = document.getElementsByClassName("comicNav")[0];
+// Get the navbar
+var navbar = document.getElementsByClassName("comicNav")[0];
+
+//Get the current xkcd number
 var xkcdNum = window.location.href.split('/')[3];
+
+// Generate the explainxkcd link
 var link = "https://explainxkcd.com/"+xkcdNum;
-element.innerHTML = element.innerHTML + "<li><a href=\"" + link + "\"> Explain</a><li>"
+
+// Add the link!
+navbar.innerHTML = navbar.innerHTML + "<li><a href=\"" + link + "\"> Explain</a><li>"
